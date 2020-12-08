14a253701239ed5e044ee5a02b6b5487be3be489
Aaron Foucheaux <afoucheaux@gmail.com>
2020-12-08 07:50:30 -0600

    inital commit, Added Unicorn Class and test passed.

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,23 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
     this.color = color;
+    }
+  }
+  isWhite = function() {
+    if (this.color !== "white") {
+      return false;
+    }
+  }
+  says = function(string) {
+    var sparklyingStuffFront = "**;*";
+    var sparklyingStuffBack = "*;**"
+    return `${sparklyingStuffFront} ${string} ${sparklyingStuffBack}`;
   }
 }
+
 
 module.exports = Unicorn;
