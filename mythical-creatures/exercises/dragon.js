09dd926b13425188fd6081af95f5b3927eda2996
Aaron Foucheaux <afoucheaux@gmail.com>
2020-12-08 09:51:18 -0600

    added dragon class and test pass

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.fed = 0;
+  }
+  greet = function() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat = function() {
+    console.log(this.fed);
+    if (this.fed > 1) {
+      this.fed = this.fed + 1;
+      this.hungry = false;
+    } else {
+      this.fed = this.fed + 1;
+      this.hungry = true;
+    }
+  }
+}
 
+module.exports = Dragon;
